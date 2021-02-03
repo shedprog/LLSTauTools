06997cc28d718795c342b086ce1986dcf4e6a198
shedprog <flynik75@gmail.com>
2021-02-03 20:20:43 +0100

    Add cmake build option

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LLSTauTools
+
+# to compile
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.19.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+mkdir build
+cd build
+cmake3 ..
+make
---
+++ bin/StauEfficiency.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <sstream>
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TGraph.h"
+#include "TTree.h"
+#include "TROOT.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TChain.h"
+#include "TMath.h"
+
+#include "TLorentzVector.h"
+
+#include "TRandom.h"
+
+#include "Config.h"
+#include "AC1B.h"
+
+
+int main(int argc, char * argv[])
+{
+  std::cout << "this is stau StauEfficiency\n";
+  return 0;
+}
