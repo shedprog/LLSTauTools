f2f50e286e5f1ddb167665f5bc93c12dbbfa7585
myshched <mykyta.shchedrolosiev@gmail.com>
2021-04-17 21:16:37 +0200

    Updated GEN-SIM check with vertexs

--- python/FWLite_GEN-SIM-RECO.py
+++ python/FWLite_GEN-SIM-RECO.py
@@ -4,20 +4,27 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    files = ['/pnfs/desy.de/cms/tier2/store/user/myshched/SUS-RunIIFall18GS-production/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/210224_130933/0000/SUS-RunIIFall18GS-00022_100.root', '/pnfs/desy.de/cms/tier2/store/user/myshched/SUS-RunIIFall18GS-production/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/210224_130933/0000/SUS-RunIIFall18GS-00022_101.root', '/pnfs/desy.de/cms/tier2/store/user/myshched/SUS-RunIIFall18GS-production/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/SUS-RunIIFall18GS_ctau0p01-1000mm_mstau100_mlsp1-RAWSIM/210224_130933/0000/SUS-RunIIFall18GS-00022_102.root']
-
+    files = ['/afs/cern.ch/user/m/myshched/STauGENProduction/test_prod_HSCP/SUS-RunIIFall18GS-00022.root']
 
     for f_name in files:
+        
+        print "FILE: ",f_name
 
         events = Events(f_name)
 
-        # Tracks check part
-        handleGEN = Handle('std::vector<reco::GenParticle>')
-        handleTracks = Handle('std::vector<SimTrack> ')
-        labelGEN = 'genParticles'
+        # Tracks info part
+        handleTracks = Handle('std::vector<SimTrack>')
         labelTracks = 'g4SimHits'
 
-        # Hists check part
+        # Gen par info
+        handleGEN = Handle('std::vector<reco::GenParticle>')
+        labelGEN = 'genParticles'
+
+        # Vertex info part
+        handleVertex = Handle('std::vector<SimVertex>')
+        labelVertex = 'g4SimHits'
+
+        # Hists info part
         handleHits = Handle('std::vector<PSimHit> ')
         labelsHits = [ "TrackerHitsPixelBarrelHighTof",
                        "TrackerHitsPixelBarrelLowTof",
@@ -26,10 +33,10 @@ if __name__ == '__main__':
 
         n_ev = 0
         for n_ev, ev in enumerate(events):
+            print "event: ", n_ev
             if n_ev % 1000 == 0:
                 print n_ev, "events"
-            # if n_ev > 10:
-            #     break
+
 
             ev.getByLabel(labelGEN, handleGEN)
             gen_particle = handleGEN.product()
@@ -37,11 +44,10 @@ if __name__ == '__main__':
             ev.getByLabel(labelTracks, handleTracks)
             tracks = handleTracks.product()
 
-            Tracker_nHits = np.zeros((4,1600000), dtype=int)
-            # Tracker_nHits_x = [[[] for j in range(1600000)] for i in range(4)]
-            # Tracker_nHits_z = [[[] for j in range(1600000)] for i in range(4)]
-            # Tracker_nHits_y = [[[] for j in range(1600000)] for i in range(4)]
+            ev.getByLabel(labelVertex, handleVertex)
+            vertex = handleVertex.product()
 
+            Tracker_nHits = np.zeros((4,1600000), dtype=int)
             for i, label in enumerate(labelsHits):
                 '''
                 In this part, hits inside PIXEL tracker
@@ -52,10 +58,14 @@ if __name__ == '__main__':
                 for hit in hits:
                     Tracker_nHits[i][hit.trackId()]+=1
 
-                    # Tracker_nHits_x[i][hit.trackId()].append(hit.entryPoint().x())
-                    # Tracker_nHits_z[i][hit.trackId()].append(hit.entryPoint().y())
-                    # Tracker_nHits_y[i][hit.trackId()].append(hit.entryPoint().z())
 
+            # for gen in gen_particle:
+            #     # if(gen.vertex().r()>=10.0):
+            #     print "pdgId:", gen.pdgId(), \
+            #         "pt:", gen.pt(), \
+            #         "vtx rho:", gen.vertex().rho(), \
+            #         "vtx abs:", gen.vertex().r()
+            
             for tr in tracks:
                 '''
                 This part checks:
@@ -64,9 +74,22 @@ if __name__ == '__main__':
                     (also added condition that momentum points outside the cms)
                 2.  If there is a track which coresponds to stau (pdgId == 1000015)
                 '''
-                dis_transv = 500.0
+                dis_transv = 100.0 #cm
 
-                if tr.genpartIndex() >= 0:
+                if not tr.noVertex():
+                    
+                    print "vertex info: "
+                    print "vertIndex: ", vertex[tr.vertIndex()].position().x(), \
+                                         vertex[tr.vertIndex()].position().y(), \
+                                         vertex[tr.vertIndex()].position().z()
+
+                    print "genPart: ", gen_particle[tr.genpartIndex()-1].vertex().x(), \
+                                       gen_particle[tr.genpartIndex()-1].vertex().y(), \
+                                       gen_particle[tr.genpartIndex()-1].vertex().z()
+
+                if not tr.noGenpart():
+    
+                    # print "genPart index: ", tr.genpartIndex()
 
                     if(gen_particle[tr.genpartIndex()-1].vertex().rho()>=dis_transv \
                        and (gen_particle[tr.genpartIndex()-1].vertex().x()<0) == (gen_particle[tr.genpartIndex()-1].momentum().x()<0) \
@@ -82,24 +105,16 @@ if __name__ == '__main__':
                             "PixelBarrelLowTof:", Tracker_nHits[1][tr.trackId()], \
                             "PixelEndcapHighTof:", Tracker_nHits[2][tr.trackId()], \
                             "PixelEndcapLowTof:", Tracker_nHits[3][tr.trackId()]
-                        # print Tracker_nHits_x[0][tr.trackId()], \
-                        #       Tracker_nHits_z[0][tr.trackId()], \
-                        #       Tracker_nHits_y[0][tr.trackId()]
-                        # print Tracker_nHits_x[1][tr.trackId()], \
-                        #       Tracker_nHits_z[1][tr.trackId()], \
-                        #       Tracker_nHits_y[1][tr.trackId()]
-                        # print Tracker_nHits_x[2][tr.trackId()], \
-                        #       Tracker_nHits_z[2][tr.trackId()], \
-                        #       Tracker_nHits_y[2][tr.trackId()]
-                        # print Tracker_nHits_x[3][tr.trackId()], \
-                        #       Tracker_nHits_z[3][tr.trackId()], \
-                        #       Tracker_nHits_y[3][tr.trackId()]
 
                     if(abs(gen_particle[tr.genpartIndex()-1].pdgId())>=1000000):
 
                         print "stau particle track!: ~~~~~~~~~~~~> ", \
                             "pdgId: ", gen_particle[tr.genpartIndex()-1].pdgId(), \
                             "vtx rho: ", gen_particle[tr.genpartIndex()-1].vertex().rho(), \
-                            "vtx abs: ", gen_particle[tr.genpartIndex()-1].vertex().r()
+                            "vtx abs: ", gen_particle[tr.genpartIndex()-1].vertex().r(), \
+                            "PixelBarrelHighTof:", Tracker_nHits[0][tr.trackId()], \
+                            "PixelBarrelLowTof:", Tracker_nHits[1][tr.trackId()], \
+                            "PixelEndcapHighTof:", Tracker_nHits[2][tr.trackId()], \
+                            "PixelEndcapLowTof:", Tracker_nHits[3][tr.trackId()]
 
-        # exit()
+            raw_input("stop")
